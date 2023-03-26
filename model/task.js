aeefbd95fa8b5407631cb1865398222c9dedefad
G-Naik <121098220+G-Naik@users.noreply.github.com>
2023-03-26 13:56:10 +0530

    Add files via upload

---
+++ model/task.js
@@ -0,0 +1,22 @@
+const mongoose = require("mongoose")
+
+let taskSchema = mongoose.Schema({
+    task:{
+        type:String,
+        required:true
+    },
+    startDate:{
+        type:String,
+        required:true
+    },
+    endDate:{
+        type:String,
+        required:true
+    },
+    prirority:{
+        type:String,
+        required:true
+    }
+})
+
+module.exports = mongoose.model("Task",taskSchema)
